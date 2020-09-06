8c6568884121957e03da5aca34555f8bf9103de4
Miguel <miguelvr98@gmail.com>
2020-09-06 17:56:24 +0200

    Arreglado el error

---
+++ entorno/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/miguel/Escritorio/Prueba Examen/EGC-830-migvelrom2/entorno/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
